2ceef14f97ef4481e52e73646ae2855a44a388b1
Vardhan02 <vardhanviswa@gmail.com>
2024-01-08 14:45:14 +0530

    1

--- Hardware (Raspberry Pi)/Servo_Test/Servo_Test.py
+++ Hardware (Raspberry Pi)/Servo_Test/Servo_Test.py
@@ -1,12 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
-servo_pin = 18  # The GPIO pin the servo is connected to
+servo_pin = 14  # The GPIO pin the servo is connected to
+servo_pin_1 = 15
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_pin, GPIO.OUT)
+GPIO.setup(servo_pin_1, GPIO.OUT)
 
-pwm = GPIO.PWM(servo_pin, 50)  # GPIO 18 for PWM with 100Hz
+pwm = GPIO.PWM(servo_pin, 50)  # GPIO 14 for PWM with 50Hz
 pwm.start(2.5)  # Initialization
+
+pwm1 = GPIO.PWM(servo_pin_1, 50)  # GPIO 15 for PWM with 50Hz
+pwm1.start(2.5)  # Initialization
 
 def get_pwm(angle):
     return (angle/18.0) + 2.5
@@ -14,7 +19,7 @@ def get_pwm(angle):
 try:
     while(1):
         '''
-        #Wrist
+        # Wrist
         # Move servo to 0 degrees
         pwm.ChangeDutyCycle(get_pwm(0))
         time.sleep(1)
@@ -22,22 +27,26 @@ try:
         # Move servo to 270 degrees
         pwm.ChangeDutyCycle(get_pwm(180))
         time.sleep(1)
-        
-        # Move servo back to 0 degrees
-        pwm.ChangeDutyCycle(get_pwm(0))
-        time.sleep(1)
-        '''
 
+        # Shouder
         # Move servo to 0 degrees
-        pwm.ChangeDutyCycle(get_pwm(0))
+        pwm.ChangeDutyCycle(get_pwm(-20))
         time.sleep(1)
         
         # Move servo to 270 degrees
-        pwm.ChangeDutyCycle(get_pwm(180))
+        pwm.ChangeDutyCycle(get_pwm(70))
         time.sleep(1)
         
-        # Move servo back to 0 degrees
-        pwm.ChangeDutyCycle(get_pwm(0))
+        '''
+        # Base
+        # Move servo to 0 degrees
+        pwm.ChangeDutyCycle(2.5)
+        #pwm.ChangeDutyCycle(get_pwm(30))
+        time.sleep(1)
+        
+        # Move servo to 270 degrees
+        pwm.ChangeDutyCycle(12.5)
+        #pwm.ChangeDutyCycle(get_pwm(60))
         time.sleep(1)
 
 except KeyboardInterrupt:
